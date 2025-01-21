482d59941707babb8b5334f578224068af78acda
Naufal Aulia <naufal7600@gmail.com>
2025-01-21 14:49:17 +0700

    feat: add response interceptor to invalidate login when token invalid

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
-import axios, { AxiosInstance } from "axios";
+import axios, { AxiosError, AxiosInstance } from "axios";
 import { AuthAPI, newAuthAPI } from './auth';
-import { LS_TOKEN } from './constants/local-storage-key';
+import { LS_TOKEN, LS_USER_DATA } from './constants/local-storage-key';
 
 class APIContainer {
   #httpClient: AxiosInstance;
@@ -28,6 +28,21 @@ httpClient.interceptors.request.use((value) => {
   value.headers.set("Authorization", `Bearer ${localStorage.getItem(LS_TOKEN)}`);
 
   return value;
+});
+
+httpClient.interceptors.response.use((value) => {
+  return value;
+},
+(error) => {
+  if (error instanceof AxiosError) {
+    if (error.response?.status === 401 && !error.response.config.url?.includes('auth')) {
+      window.alert(error.response.data?.message);
+      localStorage.removeItem(LS_TOKEN);
+      localStorage.removeItem(LS_USER_DATA);
+      window.location.assign('/auth/login');
+    }
+  }
+  throw error;
 });
 
 const api = new APIContainer(httpClient);
